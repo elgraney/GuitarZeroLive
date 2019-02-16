a4c5fd679c67b863390062f5e7d55f4cc7bdff74
Matthew Crane <elgraney501@gmail.com>
2019-02-16 13:42:10 +0000

    Worthless tinkering

--- src/com/RD/GUI/ModeTemplate.java
+++ src/com/RD/GUI/ModeTemplate.java
@@ -1,7 +1,5 @@
 package com.RD.GUI;
 
-import javafx.scene.layout.BackgroundImage;
-
 import java.awt.*;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
@@ -16,7 +14,11 @@ public class ModeTemplate extends JFrame {
 
 
     protected void setUpCarousel(){
+        Rectangle r = rootPane.getBounds();
+        int h = r.height;
+        int w = r.width;
         JPanel container = new JPanel();
+        System.out.println(w);
         container.setBounds(450, 300, 1000,  300);//make dynamic
 
         container.setBackground(Color.black);
--- src/com/RD/Main.java
+++ src/com/RD/Main.java
@@ -9,7 +9,6 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 
-import static com.sun.java.accessibility.util.AWTEventMonitor.addWindowListener;
 
 public class Main {
 
@@ -19,10 +18,13 @@ public class Main {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(1920, 1080);
         frame.setMinimumSize(new Dimension(800, 400));
-        frame.setResizable(true);
-        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        //frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
         frame.setUndecorated(true);
         frame.setVisible(true);
+
+        //have guitar highway set aspect ratio locked, to fit screen vertically, centered
+        //have high res background image
+        //have container holding 5 things locked center, relative size.
     }
 
 }
