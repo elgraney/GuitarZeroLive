4bc33f2d7c1d4febb31add018092e9120139a82f
Matthew Crane <elgraney501@gmail.com>
2019-02-20 15:22:51 +0000

    added a temp view window
    Now just add your listener to it and it should work

---
+++ src/com/RD/tempView.java
@@ -0,0 +1,22 @@
+package com.RD;
+
+import com.RD.GUI.SetUpGUI;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class tempView {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(1920, 1080);
+        frame.setMinimumSize(new Dimension(800, 400));
+        //frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        //frame.setUndecorated(true);
+        frame.setFocusable(true);
+        frame.setVisible(true);
+
+
+        //add listener here
+    }
+}
