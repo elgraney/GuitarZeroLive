60b1d4725453e836412a5020038fe602f06d7f87
wallgren500 <swallgren@gmail.com>
2019-03-07 15:26:14 +0000

    working output of button presses
    changed name of my code to guitar to avoid name conflicts, is called by main method

--- src/com/RD/GUI/Guitar.java
+++ src/com/RD/GUI/Guitar.java
@@ -1,17 +1,20 @@
-package SWPlasticGuitar;
+package com.RD.GUI;
 
 import java.awt.*;
-import javax.swing.JFrame;
-import javax.swing.JButton;
-import javax.swing.JPanel;
 
+import com.RD.GUI.ModeTemplate;
+import com.RD.GUI.SelectMode;
+import net.java.games.input.Component;
+import net.java.games.input.Controller;
+import net.java.games.input.ControllerEnvironment;
 
 
 /*
  * Plastic guitar test
  *
- * @SW
- * @version
+ * @Sophia Wallgren
+ * @ edited March 7th
+
  *
  *   $ CLASSPATH=jinput-2.0.9.jar:.
  *   $ export CLASSPATH
@@ -19,35 +22,11 @@ import javax.swing.JPanel;
  *   $ java -Djava.library.path=. PlasticGuitar
  */
 
-// use keypressed for now and then switch it for buttons
 
-public class GuitarController {
+public class Guitar {
     static final String GUITAR_HERO = "Guitar Hero";
     static final int DELAY = 150;
-    private static JButton[] buttons;
-    
-    /*
-     * Make a frame of buttons for controller components.
-     */
-    private static JFrame makeFrame( Controller ctrl ) {
-        JFrame frm = new JFrame();
-        JPanel pan = new JPanel( new GridLayout( 0, 2 ) );
-        
-        Component[] cmps = ctrl.getComponents();
-        buttons = new JButton[ cmps.length ];
-        
-        for ( int i = 0; i < buttons.length; i = i + 1 ) {
-            JButton button = new JButton();
-            button.setPreferredSize( new Dimension( 120, 40 ) );
-            buttons[ i ] = button;
-            pan.add( button );
-        }
-        
-        frm.add( pan );
-        frm.pack();
-        
-        return frm;
-    }
+    private ModeTemplate template;
     
     public static void pollForever( Controller ctrl ) {
         Component[] cmps = ctrl.getComponents();
@@ -66,29 +45,37 @@ public class GuitarController {
                         col = Color.BLUE;
                         if( i==0 || i == 4 || i == 5) {
                             System.out.println("White note  " + i + "   pressed");
+                            //Playmode.function();
                         }
                         if( i==1 || i == 2 || i == 3) {
                             System.out.println("Black note  " + i + "   pressed");
                         }
                         if (i==8) {
-                            System.out.println("Hero Power button pressed");
+                            System.out.println("Select/ Hero Power button pressed");
+                            // call hero power button function
+                            //SelectMode.onSelect();
                         }
                         if (i==9){
                             System.out.println("Button 9 presssed");
                         }
                         if (i==10){
                             System.out.println("Escape button pressed");
+                            //ModeTemplate.onEscape();
+
                         }
                         if (i ==12) {
                             System.out.println("On/off button pressed");
                         }
-                        
 
                         if(vals[15] == 1){
                             System.out.println("Strum down");
+                            //template.right();
+                            //non static field cant be refernced from static context
                         }
                         else if(vals[15] == -1){
                             System.out.println("Strum up");
+                            //ModeTemplate.left();
+                            //doing a non static method call
                         }
                         else if (vals[17]>0.1) {
                         System.out.println("Whammy bar pressed");
@@ -101,52 +88,30 @@ public class GuitarController {
                         col = Color.YELLOW;
                         if (vals[13] == 0.125 || vals[13] == 0.25 || vals[13] == 0.375 || vals[13] == 0.5) {
                             System.out.println("Guitar Strum up");
+                            // call function for strummming
                         }
                         if (vals[13] == 0.625 || vals[13] == 0.75 || vals[13] == 0.875 || vals[13] == 1.0) {
                             System.out.println("Guitar Strum down");
+                            // call function for strummming
                         }
                     }
-                    buttons[ i ].setBackground( col );
-                    //buttons[ i ].setText( "" + val + "   button   "+ i);
-                    buttons[ i ].setOpaque( true );
-                    buttons[ i ].repaint();
                 }
             }
-            // button names
-            buttons[ 15 ].setText(  "  Strum bar       " + 15);
-            buttons[ 8 ].setText (  "  Hero Power Button " + 8);
-            buttons[ 10 ].setText(  "  Escape button   " + 10 );
-            buttons[ 12 ].setText(  "  On/off button   " + 12 );
-            buttons[ 13 ].setText(  "  Strum button " + 13);
-            buttons[ 17 ].setText(  "  Whammy bar  " + 17 );
-            buttons[ 16 ].setText(  "  Guitar position  " + 16 );
-            
-            
-            buttons[ 1 ].setText(  "  Black note 1   " + 1 );
-            buttons[ 2 ].setText(  "  Black note 2   " + 2 );
-            buttons[ 3 ].setText(  "  Black note 3   " + 3 );
-            buttons[ 0 ].setText(  "  White note 1   " + 0 );
-            buttons[ 4 ].setText(  "  White note 2   " + 4 );
-            buttons[ 5 ].setText(  "  White note 3   " + 5 );
-            
-            
+           
             try { /* delay */
                 Thread.sleep( DELAY );
             } catch ( Exception exn ) {
                 System.out.println( exn ); System.exit( 1 );
             }
-        }
-        
+        }   
     }
     
-    public static void main(String[] argv) {
+    public static void run() {
         ControllerEnvironment cenv  = ControllerEnvironment.getDefaultEnvironment();
         Controller[]          ctrls = cenv.getControllers();
         
         for ( Controller ctrl : ctrls ) {
             if ( ctrl.getName().contains( GUITAR_HERO ) ) {
-                JFrame frm = makeFrame( ctrl );
-                frm.setVisible( true );
                 pollForever( ctrl );
             }
         }
@@ -154,5 +119,4 @@ public class GuitarController {
         System.out.println( " controller not found" );
         System.exit( 1 );
     }
-    
 }
